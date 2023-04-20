17311adb6567b3e806f9ec6379b86817352314ff
Dominic <andro5@web.de>
2023-04-20 12:29:05 +0200

    add "how are you" in console

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,6 +3,7 @@ package de.neuefische;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        
+
+        System.out.println("Wie gehts dir?");
     }
 }
